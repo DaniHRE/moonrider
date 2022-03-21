d9b3d52c96ba6368e48d99825c898a3ff80692c9
Daniel Henrique <henriqueevaldo@outlook.com>
2022-03-21 20:16:19 -0300

    start message added

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,8 @@ const color = require("./assets/colors.js");
 const banner = require("./assets/banner.js")
 const cfonts = require("cfonts");
 
+colorful = (color, string, reset = '\x1b[5m') => color + string + reset;
+
 client.config = config;
 client.cooldowns = new Discord.Collection();
 
@@ -13,14 +15,18 @@ const loadEvents = require('./modules/loadEvents');
 const loadCommands = require('./modules/loadCommands');
 const loadPlayer = require('./modules/loadPlayer');
 
-client.login(config.token); 
+client.login(config.token);
 
 client.once('ready', async () => {
-    // console.log("=================================")
-    // console.log("✅ - Moonrider 🌑🩸 Online - ✅")
-    // console.log("=================================")
+    console.log(colorful(color.branco, "\n=================================")),
+    console.log(colorful(color.preto, "✅ - Moonrider 🌑🩸",(colorful(color.blink, color.verde, " Online - ✅")))),
+    console.log(colorful(color.branco, "================================="))
 
-    console.log(banner.string);
+    console.log(colorful(color.bright, color.vermelho, `\n⊱ ============ ⊱ [LOGS] ⊰ ============ ⊰`)),
+    console.log(colorful(color.amarelo, `[LOGS] ${client.user.tag} Está online! `)),
+    console.log(colorful(color.verde, `[LOGS] Estou em ${client.guilds.cache.size} servidores.`)), 
+    console.log(colorful(color.azul, `[LOGS] Cuidando de ${client.users.cache.size} membros.`))
+
 })
 
 loadEvents(client);  
