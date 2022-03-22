8773f96d98616f4ee1bd86c671d20f3abd33619d
Daniel Henrique <henriqueevaldo@outlook.com>
2022-03-22 19:55:56 -0300

    added player features

---
+++ src/commands/player/nowplaying.js
@@ -0,0 +1,42 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+    name: 'nowplaying',
+    aliases: ['np'],
+    utilisation: '{prefix}nowplaying',
+    voiceChannel: true,
+
+    run(client, message) {
+        const queue = player.getQueue(message.guild.id);
+
+        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+
+        const track = queue.current;
+
+        const embed = new MessageEmbed();
+
+        embed.setColor('RED');
+        embed.setThumbnail(track.thumbnail);
+        embed.setAuthor(track.title, client.user.displayAvatarURL({ size: 1024, dynamic: true }));
+
+        const methods = ['disabled', 'track', 'queue'];
+
+        const timestamp = queue.getPlayerTimestamp();
+        const trackDuration = timestamp.progress == 'Infinity' ? 'infinity (live)' : track.duration;
+
+        embed.setDescription(`Volume **${queue.volume}**%\nDuration **${trackDuration}**\nLoop mode **${methods[queue.repeatMode]}**\nRequested by ${track.requestedBy}`);
+
+        embed.setTimestamp();
+        embed.setFooter('Music comes first - Made with heart by Zerio ❤️', message.author.avatarURL({ dynamic: true }));
+
+        const saveButton = new MessageButton();
+
+        saveButton.setLabel('Save this track');
+        saveButton.setCustomId('saveTrack');
+        saveButton.setStyle('SUCCESS');
+
+        const row = new MessageActionRow().addComponents(saveButton);
+
+        message.channel.send({ embeds: [embed], components: [row] });
+    },
+};
