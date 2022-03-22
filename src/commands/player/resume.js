4ad1cc11d6612ff27af37057526ecb2ae88837b9
Daniel Henrique <henriqueevaldo@outlook.com>
2022-03-22 19:55:21 -0300

    added principal player functions

---
+++ src/commands/player/resume.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'resume',
+    aliases: ['rs'],
+    utilisation: '{prefix}resume',
+    voiceChannel: true,
+
+    run(client, message) {
+        const queue = player.getQueue(message.guild.id);
+
+        if (!queue) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+
+        const success = queue.setPaused(false);
+
+        return message.channel.send(success ? `Current music ${queue.current.title} resumed ✅` : `Something went wrong ${message.author}... try again ? ❌`);
+    },
+};
---
+++ src/commands/player/skip.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'skip',
+    aliases: ['sk'],
+    utilisation: '{prefix}skip',
+    voiceChannel: true,
+
+    run(client, message) {
+        const queue = player.getQueue(message.guild.id);
+
+        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+
+        const success = queue.skip();
+
+        return message.channel.send(success ? `Current music ${queue.current.title} skipped ✅` : `Something went wrong ${message.author}... try again ? ❌`);
+    },
+};
