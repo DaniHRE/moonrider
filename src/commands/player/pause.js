4a759d1a78d93edb0ffc95ce445b63bcad30f275
Daniel Henrique <henriqueevaldo@outlook.com>
2022-03-22 19:54:49 -0300

    added based players commands

---
+++ src/commands/player/pause.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'pause',
+    aliases: [],
+    utilisation: '{prefix}pause',
+    voiceChannel: true,
+
+    run(client, message) {
+        const queue = player.getQueue(message.guild.id);
+
+        if (!queue) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+
+        const success = queue.setPaused(true);
+
+        return message.channel.send(success ? `Current music ${queue.current.title} paused ✅` : `Something went wrong ${message.author}... try again ? ❌`);
+    },
+};
---
+++ src/commands/player/stop.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'stop',
+    aliases: ['dc'],
+    utilisation: '{prefix}stop',
+    voiceChannel: true,
+
+    run(client, message) {
+        const queue = player.getQueue(message.guild.id);
+
+        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+
+        queue.destroy();
+
+        message.channel.send(`Music stopped into this server, see you next time ✅`);
+    },
+};
