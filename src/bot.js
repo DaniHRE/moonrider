25831df62ad12329853384b60eb98bf845dee1a3
Daniel Henrique <henriqueevaldo@outlook.com>
2022-03-22 19:54:21 -0300

    added discord-player, ffmpeg, ms and opus

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
-const Discord = require("discord.js"); 
+const Discord = require("discord.js");
+const { Player } = require("discord-player");
 const client = new Discord.Client({intents: 32767});
-const config = require("../config.json"); 
+const config = require("../config"); 
 
 const color = require("./assets/colors.js");
 const banner = require("./assets/banner.js")
@@ -11,24 +12,24 @@ colorful = (color, string, reset = '\x1b[5m') => color + string + reset;
 client.config = config;
 client.cooldowns = new Discord.Collection();
 
+global.player = new Player(client, config.opt.discordPlayer)
+
 const loadEvents = require('./modules/loadEvents');
 const loadCommands = require('./modules/loadCommands');
-const loadPlayer = require('./modules/loadPlayer');
+// const loadPlayer = require('./modules/loadPlayer');
 
 client.login(config.token);
 
 client.once('ready', async () => {
     console.log(colorful(color.branco, "\n=================================")),
-    console.log(colorful(color.preto, "✅ - Moonrider 🌑🩸",(colorful(color.blink, color.verde, " Online - ✅")))),
+    console.log(colorful(color.bright, "✅ - Moonrider 🌑🩸",(colorful(color.blink, color.verde, " Online - ✅")))),
     console.log(colorful(color.branco, "================================="))
-
     console.log(colorful(color.bright, color.vermelho, `\n⊱ ============ ⊱ [LOGS] ⊰ ============ ⊰`)),
     console.log(colorful(color.amarelo, `[LOGS] ${client.user.tag} Está online! `)),
     console.log(colorful(color.verde, `[LOGS] Estou em ${client.guilds.cache.size} servidores.`)), 
     console.log(colorful(color.azul, `[LOGS] Cuidando de ${client.users.cache.size} membros.`))
-
 })
 
 loadEvents(client);  
 loadCommands(client);
-loadPlayer(client);
+// loadPlayer(client);
