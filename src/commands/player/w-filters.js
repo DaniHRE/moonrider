cc524f9ea1432dd021476f049b1ed6dbbe50bebe
Daniel Henrique <henriqueevaldo@outlook.com>
2022-03-26 12:58:11 -0300

    change w-filters syntax

--- src/commands/player/w-filters.js
+++ src/commands/player/w-filters.js
@@ -1,11 +1,13 @@
 module.exports = {
     name: 'w-filters',
-    execute(message, args) {
+    usage: "",
+
+    run (client, message, args) {
         if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel !`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`You are not in the same voice channel !`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`No music currently playing !`);
+        if (!player.getQueue(message)) return message.channel.send(`No music currently playing !`);
 
         const filtersStatuses = [[], []];
 
@@ -14,17 +16,15 @@ module.exports = {
             array.push(filterName.charAt(0).toUpperCase() + filterName.slice(1) + " : " + (client.player.getQueue(message).filters[filterName] ? "success" : "off"));
         });
 
-        message.channel.send({
-            embed: {
-                color: 'ORANGE',
-                footer: { text: 'This bot uses a Github project made by Zerio (ZerioDev/Music-bot)' },
-                fields: [
-                    { name: 'Filters', value: filtersStatuses[0].join('\n'), inline: true },
-                    { name: '** **', value: filtersStatuses[1].join('\n'), inline: true },
-                ],
-                timestamp: new Date(),
-                description: `List of all filters enabled or disabled.\nUse \`${client.config.prefix}filter\` to add a filter to a song.`,
-            },
-        });
+        let embed = new Discord.Embed()
+            .setColor("RANDOM")
+            .setAuthor({name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true })})
+            .setFooter({text: 'By Dino💓'})
+            .addFields(
+                {name:'Filters', value: filtersStatuses[0].join('\n'), inline: true},
+                {name: '** **', value: filtersStatuses[1].join('\n'), inline: true })
+            .setTimestamp(new Date())
+            .setDescription(`List of all filters enabled or disabled.\nUse \`${client.config.prefix}filter\` to add a filter to a song.`)
+        message.channel.send({embeds: {embed}});
     },
 };
