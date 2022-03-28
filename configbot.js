074709b0e628aa4b25dab9c658d7dd9a2e8758ca
Daniel Henrique <henriqueevaldo@outlook.com>
2022-03-28 19:35:40 -0300

    added 'config.js' to example

---
+++ configbot.js
@@ -0,0 +1,27 @@
+module.exports = {
+    token: "OTU0MTUwMzI0ODAzODk1MzE2.YjO7vA.zXOmLCQ5whSmUKt42cvlJARqZf8",
+    prefix: "!",
+
+        app: {
+            px: "!",
+            token: "OTU0MTUwMzI0ODAzODk1MzE2.YjO7vA.zXOmLCQ5whSmUKt42cvlJARqZf8",
+            playing: "Use !help - Lancelot Beserker 🌑🩸",
+            filters: ["8D", "gate", "haas", "phaser", "treble", "tremolo", "vibrato", "karaoke", "flanger", "mcompand", "pulsator", "subboost", "bassboost", "vaporwave", "nightcore", "normalizer", "surrounding"]
+        },
+        opt: {
+            DJ: {
+                enabled: false,
+                roleName: "DJ",
+                commands: ["back", "clear", "filter", "loop", "pause", "resume", "seek", "shuffle", "skip", "stop", "volume"]
+            },
+            maxVol: 100,
+            loopMessage: false,
+            discordPlayer: {
+                ytdlOptions: {
+                    quality: "highestaudio",
+                    highWaterMark: 1 << 25
+                }
+            }
+        }
+
+    }
