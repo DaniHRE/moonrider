744634c3ffcf5bd45b535696bb2b628ab3031378
Daniel Henrique <henriqueevaldo@outlook.com>
2022-04-13 19:54:57 -0300

    clear command fixed

--- src/commands/utils/clear.js
+++ src/commands/utils/clear.js
@@ -2,38 +2,37 @@ const Discord = require("discord.js");
 const { Permissions } = require("discord.js");
 
 module.exports = {
-  name: "clear",
-  usage: "",
-  permissions: Permissions.FLAGS.MANAGE_MESSAGES,
+    name: "clear",
+    description: "",
+    permissions: Permissions.FLAGS.MANAGE_MESSAGES,
 
-  run: async (client, message, args) => {
-    const msgCount = parseInt(args[0], 10);
+    run: async(client, message, args) => {
+        const msgCount = parseInt(args[0], 10);
 
-    if (!msgCount || msgCount < 1 ||msgCount > 99)
-      return message.channel.send(`:x: | ${message.author} Insira um numero entre 1 - 99.`);
+        if (!msgCount || msgCount < 1 || msgCount > 99)
+            return message.channel.send(`:x: | ${message.author} Insira um numero entre 1 - 99.`);
 
-    const cleaningMsg = await message.channel.messages.fetch({
-      limit: msgCount + 1,
-    })
-    message.channel.bulkDelete(cleaningMsg);
+        const cleaningMsg = await message.channel.messages.fetch({
+            limit: msgCount + 1,
+        })
+        message.channel.bulkDelete(cleaningMsg);
 
-    let clearMsg = new Discord.MessageEmbed()
-      .setColor("RANDOM")
-      .setDescription(`** Limpando Mensagens... 🧹**`)
-      .setFooter({text: `By Dino ❤️`, iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`})
-      .setTimestamp(Date.now());
+        let clearMsg = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setDescription(`** Limpando Mensagens... 🧹**`)
+            .setFooter({ text: `By Dino ❤️`, iconURL: `${client.user.displayAvatarURL({ dynamic: true })}` })
+            .setTimestamp(Date.now());
 
-    let clearEmbed = await message.reply({ embeds: [clearMsg] }).then(msg => {
-      setTimeout( () => {
-        let clearedMsg = new Discord.MessageEmbed()
-          .setColor("RANDOM")
-          .setDescription(`✅ | ${message.author} apagou \`${msgCount}\` mensagens!`)
-          .setFooter({ text: `By Dino ❤️`, iconURL: `${client.user.displayAvatarURL({ dynamic: true })}` })
-          .setTimestamp(Date.now());
-  
-        msg.edit({ embeds: [clearedMsg] })
-      }, 2000)
-    });
+        let messageBot = await message.channel.send({ embeds: [clearMsg] })
 
-  }
+        let cleanedMessages = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setDescription(`✅ | ${message.author} apagou \`${cleaningMsg.size}\` mensagens!`)
+            .setFooter({ text: `By Dino ❤️`, iconURL: `${client.user.displayAvatarURL({ dynamic: true })}` })
+            .setTimestamp(Date.now());
+
+        setTimeout(() => {
+            messageBot.edit({embeds: [cleanedMessages]});
+        }, 2000)
+    }
 }
